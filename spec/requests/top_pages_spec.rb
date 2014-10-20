ab421fdf7671ab36eb48fa3f9a541511544dc3d4
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-20 17:53:39 +0900

    トップページのテスト修正

--- spec/requests/top_pages_spec.rb
+++ spec/requests/top_pages_spec.rb
@@ -3,14 +3,36 @@ require "webmock/rspec"
 
 WebMock.allow_net_connect!
 
-RSpec.describe "TopPages", :type => :request do
+EXAMPLE_SITE_URL = "https://api.instagram.com/v1/tags/Iceland/media/recent.json"
 
-  subject { page }
+RSpec.describe 'ユーザーが写真一覧を閲覧する', :type => :request do
 
-  describe "GET /top_pages" do
-    it "works! (now write some real specs)" do
-      visit root_path
-      should have_title('Worldgram')
+  context 'トップページに遷移したとき' do
+
+    context 'かつ InstagramAPIからデータが取得できたとき' do
+      before do
+        body = [{
+          :images => {
+            :low_resolution => { :url => 'http://example.jpg' }
+          },
+          :caption => { :text => "hogehoge" }
+        }]
+
+        stub_request(:get, EXAMPLE_SITE_URL).to_return({
+          :body => {:data => body },
+          :status => 200
+        })
+
+        visit root_path
+      end
+
+      it '写真の一覧が表示されている事' do
+        expect(page).to have_title('Worldgram')
+        expect(page).to have_selector("img[src$='http://example.jpg']")
+        expect(page).to have_selector('div.caption', text: 'hogehoge')
+      end
+
     end
   end
+
 end
