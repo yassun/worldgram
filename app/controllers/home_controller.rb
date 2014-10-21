9acafc0a95be4f034c55112374b8ea0152e96839
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-21 19:58:44 +0900

    counry_nameをランダムで取得する際に0が取得されないように修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @country_name = CountryName.find(rand(CountryName.count))
+    @country_name = CountryName.find(rand(CountryName.count) + 1)
     @medias = Instagram.tag_recent_media(URI.encode(@country_name.en), :count => 18)
   end
 end
