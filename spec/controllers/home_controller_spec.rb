355f55eb9bc5e05b9029e250b25f6e971dcb9f09
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-22 19:22:28 +0900

    controllerのテスト追加

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
+require "webmock/rspec"
+
+WebMock.disable_net_connect!(:allow_localhost => true)
+INSTAGRAM_API_URL = /.*api\.instagram\.com.*/
 
 RSpec.describe HomeController, :type => :controller do
 
   describe "GET index" do
+
+    before do
+      FactoryGirl.create(:country_name)
+      body = [{
+        :images => {
+          :low_resolution => { :url => 'http://example.jpg' }
+        },
+        :caption => { :text => "hogehoge" }
+      }]
+
+      stub_request(:get, INSTAGRAM_API_URL).to_return({
+        :body => {:data => body },
+        :status => 200
+      })
+    end
+
     it "returns http success" do
-      get :index
+      xhr :get, :index
       expect(response).to have_http_status(:success)
     end
   end
-
 end
+
