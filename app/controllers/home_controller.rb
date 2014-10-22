c3c6760f2de9ece74854c09140bf16c44d0c9013
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-22 17:41:07 +0900

    Randamボタン押下時のAjax処理を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     @country_name = CountryName.find(rand(CountryName.count) + 1)
     @medias = Instagram.tag_recent_media(URI.encode(@country_name.en), :count => 18)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
 
